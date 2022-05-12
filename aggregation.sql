1d23d60aadfe6591a0826d618fbc5aec1eda3524
srmono <bvsrao91@gmail.com>
2022-05-12 09:18:17 +0530

    updated agg... queries

--- aggregation.sql
+++ aggregation.sql
@@ -246,3 +246,137 @@ db.persons.aggregate([
     { $sort: {numPersons: -1}}
 ]).pretty()
 
+---------------------------------------------
+
+-- Push array data into db
+-- use array-data.json
+
+-- db.friends.insertMany([ "copy data from array-data.json and paste here" ])
+
+db.friends.aggregate([
+    {$group: {_id: {age: "$age"}, allHobbies: { $push: "$hobbies" }  }}
+]).pretty()
+
+-- using unwind
+
+db.friends.aggregate([
+    { $unwind: "$hobbies" },
+    {$group: {_id: {age: "$age"}, allHobbies: { $push: "$hobbies" }  }}
+]).pretty()
+
+
+-- remove dupliates in output
+db.friends.aggregate([
+    { $unwind: "$hobbies" },
+    {$group: {_id: {age: "$age"}, allHobbies: { $addToSet: "$hobbies" }  }}
+]).pretty()
+
+
+
+-- using projection with arrays
+db.friends.aggregate([
+    { $project: { _id: 0, examScore: { $slice: [ "$examScores", 1 ] } }}
+]).pretty()
+
+db.friends.aggregate([
+    { $project: { _id: 0, examScore: { $slice: [ "$examScores", -1 ] } }}
+]).pretty()
+
+db.friends.aggregate([
+    { $project: { _id: 0, examScore: { $slice: [ "$examScores", 2, 1 ] } }}
+]).pretty()
+
+-- length of an array
+
+db.friends.aggregate([
+    { $project: { _id: 0, numScores: { $size: "$examScores" } }}
+]).pretty()
+
+
+--Using filters
+
+db.friends.aggregate([
+    { 
+        $project: { 
+            _id: 0,
+            scores: {
+                $filter: {
+                    input: '$examScores',
+                    as: "sc",
+                    cond: {
+                        $gt: [ "$$sc.score", 60 ]
+                    }
+                }
+            }
+        }
+    }
+]).pretty()
+
+-- apply multiple operators on array
+
+db.friends.aggregate([
+    { $unwind: "$examScores"},
+    { $sort : {examScores: -1}}
+]).pretty()
+
+
+db.friends.aggregate([
+    { $unwind: "$examScores"},
+    { $project: { _id: 1, name: 1, age: 1, score: "$examScores.score"} },
+    { $sort : {score: -1}},
+    { $group: { _id: "$_id", name: { $first: "$name"} , maxScore: { $max: "$score"} }},
+    { $sort: { maxScore: -1}}
+]).pretty()
+
+
+-----------------------------------------------------
+
+-- $bucket 
+
+db.persons.findOne()
+
+db.persons.aggregate([
+    {
+        $bucket: {
+            groupBy: "$dob.age",
+            boundaries: [0, 10, 30, 50, 80, 120],
+            output: {
+                numPersons: { $sum: 1},
+                averageAge: { $avg: "$dob.age"}
+            }
+        }
+    }
+]).pretty()
+
+> db.persons.find({ "dob.age": {$lt: 18} })
+> db.persons.find({ "dob.age": {$gt: 80} })
+>
+
+db.persons.aggregate([
+    {
+        $bucket: {
+            groupBy: "$dob.age",
+            boundaries: [18, 30, 40, 50, 60, 120],
+            output: {
+                numPersons: { $sum: 1},
+                averageAge: { $avg: "$dob.age"}
+            }
+        }
+    }
+]).pretty()
+
+
+-- auto bucketing
+
+db.persons.aggregate([
+    {
+        $bucketAuto: {
+            groupBy: "$dob.age",
+            buckets: 5,
+            output: {
+                numPersons: { $sum: 1},
+                averageAge: { $avg: "$dob.age"}
+            } 
+        }
+    }
+]).pretty()
