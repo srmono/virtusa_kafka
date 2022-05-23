e9a4a8c52740bc8ccecede6e70c5b480c287e74e
srmono <bvsrao91@gmail.com>
2022-05-23 16:54:53 +0530

    spring updated

--- corejava/springcore/src/main/java/com/springcore/App.java
+++ corejava/springcore/src/main/java/com/springcore/App.java
@@ -1,5 +1,8 @@
 package com.springcore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,7 +11,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-         
+        //System.out.println( "Hello World!" );
+    	ApplicationContext context =
+    			 new ClassPathXmlApplicationContext("config.xml");
+    	
+        Student student1 = (Student) context.getBean("student1");
+        System.out.println(student1);
     }
 }
