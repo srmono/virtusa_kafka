71f15ea78a2cc64dab25d8f1b0ebf9ac246f1313
srmono <bvsrao91@gmail.com>
2022-06-03 07:05:46 +0530

    kafka connect compose file

--- projects/cloud-gateway/src/main/java/com/virtusa/cloudgateway/CloudGatewayApplication.java
+++ projects/cloud-gateway/src/main/java/com/virtusa/cloudgateway/CloudGatewayApplication.java
@@ -2,12 +2,16 @@ package com.virtusa.cloudgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+
+//@EnableHystrix
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrix
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
--- projects/cloud-gateway/src/main/java/com/virtusa/cloudgateway/FallBackMethodController.java
+++ projects/cloud-gateway/src/main/java/com/virtusa/cloudgateway/FallBackMethodController.java
@@ -4,16 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public Class FallBackMethodController{
-	
-	@GetMapping("/empServiceFallback")
-	public String empServiceFallBackMethod() {
-		return "Employee Service is taking longer than expected. Please try again later";
-	}
-	
-	@GetMapping("/departmentServiceFallback")
-	public String departmentServiceFallBackMethod() {
-		return "Department Service is taking longer than expected. Please try again later";
-	}
-	
-} 
+public class FallBackMethodController {
+
+    @GetMapping("/userServiceFallBack")
+    public String userServiceFallBackMethod() {
+        return "User Service is taking longer than Expected." +
+                " Please try again later";
+    }
+
+    @GetMapping("/departmentServiceFallBack")
+    public String departmentServiceFallBackMethod() {
+        return "Department Service is taking longer than Expected." +
+                " Please try again later";
+    }
+}
