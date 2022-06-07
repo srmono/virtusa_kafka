d0a8647488935b6f02243bc36cfb249c33a21edc
srmono <bvsrao91@gmail.com>
2022-06-07 16:58:47 +0530

    producer updated

--- spring_kafka_project/kafka-core-producer/src/main/java/com/virtusa/kafka/KafkaCoreProducerApplication.java
+++ spring_kafka_project/kafka-core-producer/src/main/java/com/virtusa/kafka/KafkaCoreProducerApplication.java
@@ -1,13 +1,27 @@
 package com.virtusa.kafka;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.virtusa.kafka.producer.HelloKafkaProducer;
+
 @SpringBootApplication
-public class KafkaCoreProducerApplication {
+public class KafkaCoreProducerApplication implements CommandLineRunner  {
+	
+	@Autowired
+	private HelloKafkaProducer producer;
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaCoreProducerApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		producer.sendHello("Virtusa " + ThreadLocalRandom.current().nextInt());
+	}
+	
 }
